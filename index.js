2c7414c7a4e45ce67248c26d5580af2b9e0b7342
wandieinnocents <wandieinnocent2@gmail.com>
2022-06-07 19:35:19 +0300

    Update

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // require used to include a module
 
 const http = require('http');
+var url = require('url');
 // import date module
 var date_time = require('./date');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// req = request , res=response
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write("The date and time are currently: " + date_time.myDateTime());
-  res.end();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.write("The date and time are currently: " + date_time.myDateTime());
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 });
 
 server.listen(port, hostname, () => {
