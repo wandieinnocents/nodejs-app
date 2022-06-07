f2a0d3bd3f262887e8f8fffd714dd6d4c000e667
wandieinnocents <wandieinnocent2@gmail.com>
2022-06-07 19:55:16 +0300

    Test upload

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const http = require('http');
 var url    = require('url');
 var fs     = require('fs');
+var formidable = require('formidable');
+var nodemailer = require('nodemailer');
+
+
 // import date module
 var date_time = require('./date');
 
@@ -11,12 +15,29 @@ const port = 3000;
 
 // req = request , res=response
 const server = http.createServer((req, res) => {
- res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write('</form>');
-  return res.end();
+
+
+ if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+
+
 });
 
 server.listen(port, hostname, () => {
